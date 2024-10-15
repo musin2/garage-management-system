093e12241e1b4446ace08bb5efc81123d5061eb7
Philip <membaphilip1@gmail.com>
2024-10-15 12:27:56 +0300

    added gitignore

--- server/debug.py
+++
@@ -1,6 +0,0 @@
-from app import app
-from models import db, User, Vehicle, Service, Appointment, ServiceVehicle
-
-if __name__ == '__main__':
-    with app.app_context():
-        import ipdb; ipdb.set_trace()
